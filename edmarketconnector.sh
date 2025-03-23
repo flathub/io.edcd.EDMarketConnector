c0b8e27274466d1d82ab41ade473cd2d696cd824
David Muckle <dvdmuckle@dvdmuckle.xyz>
2025-03-23 19:11:47 -0400

    Don't need to rm log file, tee overwrites anyways

--- edmarketconnector.sh
+++ edmarketconnector.sh
@@ -2,4 +2,4 @@
 
 export TMPDIR="${XDG_RUNTIME_DIR}/app/${FLATPAK_ID}"
 
-rm "${XDG_DATA_HOME}/EDMarketConnector/logs/EDMarketConnector.log"; cd /app/edmarketconnector; python3 EDMarketConnector.py 2>&1 | tee "${XDG_DATA_HOME}/EDMarketConnector/logs/EDMarketConnector.log"
+cd /app/edmarketconnector; python3 EDMarketConnector.py 2>&1 | tee "${XDG_DATA_HOME}/EDMarketConnector/logs/EDMarketConnector.log"
