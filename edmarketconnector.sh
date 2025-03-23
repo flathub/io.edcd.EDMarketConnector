85acf04481771c2ce710fdad7064898624d78f78
David Muckle <dvdmuckle@dvdmuckle.xyz>
2025-03-23 13:34:48 -0400

    Pipe stdout to log file
    This is to achieve parity with the windows version of EDMC.

--- edmarketconnector.sh
+++ edmarketconnector.sh
@@ -2,4 +2,5 @@
 
 export TMPDIR="${XDG_RUNTIME_DIR}/app/${FLATPAK_ID}"
 
-cd /app/edmarketconnector; python3 EDMarketConnector.py
+cd /app/edmarketconnector; python3 EDMarketConnector.py 2>&1 | tee "${XDG_DATA_HOME}/EDMarketConnector/logs/EDMarketConnector.log"
+rm "${XDG_DATA_HOME}/EDMarketConnector/logs/EDMarketConnector.log"
